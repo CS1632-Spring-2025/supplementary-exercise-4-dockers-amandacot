46934eb549fdf653fe3b4d921f3383a55915d93d
amandacot <67836328+amandacot@users.noreply.github.com>
2025-04-08 13:14:20 -0400

    Update ConnectTest.java

--- selenium/src/test/java/edu/pitt/cs/ConnectTest.java
+++ selenium/src/test/java/edu/pitt/cs/ConnectTest.java
@@ -29,6 +29,7 @@ import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ConnectTest {
