6d9563a7ffc15de52a98b955c9a5496d7b3587f6
amandacot <67836328+amandacot@users.noreply.github.com>
2025-04-08 13:10:05 -0400

    Update D3Test.java

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -29,12 +29,13 @@ public class D3Test {
   @Before
   public void setUp() {
     // driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
     
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     driver = new ChromeDriver(options);
+
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<String, Object>();
   }
   @After
   public void tearDown() {
